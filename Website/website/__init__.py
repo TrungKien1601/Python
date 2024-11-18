dcd1ad125b0b1cdfa3bfd30e103345e69032a8d7
Nguyễn Trung Kiên <164984520+TrungKien1601@users.noreply.github.com>
2024-11-18 18:27:17 +0700

    Add files via upload

---
+++ Website/website/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '1234'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/database_name'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html')
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Customer.query.get(int(id))
+
+    from .views import views
+    from .auth import auth
+    from .admin import admin
+    from .models import Customer, Cart, Product, Order
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(admin, url_prefix='/')
+
+    with app.app_context():
+        db.create_all()
+
+    return app
